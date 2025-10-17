648dcc5b8c9ae2bfc7f7e6f9e740b6dac5c2a2d9
Jesus Calvo <jesus.jesuscalvojr@gmail.com>
2025-10-17 10:23:59 -0500

    Debugged final output issues

--- doctor_tree.py
+++ doctor_tree.py
@@ -1,12 +1,57 @@
 class DoctorNode:
-    pass # Delete this line and implement the DoctorNode class
-
-
+    def __init__(self,name):
+        self.name = name
+        self.left = None
+        self.right = None
 
 class DoctorTree:
-    pass # Delete this line and implement the DoctorTree class
+    def __init__(self,root_name = None):
+        if root_name:
+            self.root = DoctorNode(root_name)
+        else:
+            self.root = None
+    
+    def insert(self,parent_node,doctor_name,side = "left"):
+        new_node = DoctorNode(doctor_name)
+        if side == 'left':
+            if parent_node.left == None:
+                parent_node.left = new_node
+            else:
+                print(f"Left child of {parent_node.name} is already in use")
+        elif side == "right":
+            if parent_node.right == None:
+                parent_node.right = new_node
+            else:
+                print(f"Right child of {parent_node.name} is already in use")
+    
+    def preorder(self,node):
+        if node:
+            print(node.name)
+            self.preorder(node.left)
+            self.preorder(node.right)
+    def inorder(self,node):
+        if node:
+            self.inorder(node.left)
+            print(node.name)
+            self.inorder(node.right)
+    def postorder(self,node):
+        if node:
+            self.postorder(node.left)
+            self.postorder(node.right)
+            print(node.name)
 
+tree = DoctorTree()
+tree.root = DoctorNode("Dr. Croft") 
+tree.insert(tree.root, "Dr. Goldsmith", "right")
+tree.insert(tree.root, "Dr. Phan", "left")
+tree.insert(tree.root.left, "Dr. Morgan", "left") 
+tree.insert(tree.root.left, "Dr. Carson", "right")
 
+print("Preorder Traversal:")
+tree.preorder(tree.root)
 
+print("Inorder Traversal:")
+tree.inorder(tree.root)
 
-# Test your DoctorTree and DoctorNode classes here
+print("Postorder Traversal:")
+tree.postorder(tree.root)
--- emergency_queue.py
+++ emergency_queue.py
@@ -1,12 +1,77 @@
 class Patient:
-    pass # Delete this line and implement the Patient class
+    def __init__(self,name,urgency):
+        self.name = name
+        self.urgency = urgency
 
 
 
 class MinHeap:
-    pass # Delete this line and implement the MinHeap class
+    def __init__(self):
+        self.data = []
 
 
+    def heapify_up(self,index):
+        while index > 0:
+            parent_index = (index - 1) // 2
+            if self.data[index].urgency < self.data[parent_index].urgency:
+                self.data[index], self.data[parent_index] =  self.data[parent_index], self.data[index]
+                index = parent_index
+            else:
+                break
+    
+    def heapify_down(self,index):
+        size = len(self.data)
+        while index < size:
+            left = 2 * index + 1
+            right = 2 * index + 2
+            smallest = index
 
+            if left < len(self.data) and self.data[left].urgency < self.data[smallest].urgency:
+                smallest = left
 
-# Test your MinHeap class here including edge cases
+            if right < len(self.data) and self.data[right].urgency < self.data[smallest].urgency:
+                smallest = right
+            
+            if smallest != index:
+                self.data[index], self.data[smallest] = self.data[smallest], self.data[index]
+                index = smallest
+            else:
+                break 
+            
+    def insert(self,patient):
+        self.data.append(patient)
+        self.heapify_up(len(self.data) - 1)
+
+    def print_heap(self):
+        for patient in self.data:
+            print(f"{patient.name}'s urgency level is {patient.urgency} ")
+    def peek(self):
+        if self.data:
+            return self.data[0]
+    def remove_min(self):
+        if not self.data:
+            return None
+        if len(self.data) == 1:
+            return self.data.pop()
+        
+        min_value = self.data[0] 
+        self.data[0] = self.data.pop() 
+        self.heapify_down(0) 
+        return min_value
+
+heap = MinHeap()
+heap.insert(Patient("Jordan", 3))
+heap.insert(Patient("Taylor", 1))
+heap.insert(Patient("Avery", 5))
+heap.insert(Patient("Jesus", 0))
+heap.insert(Patient("Lucia", 4))
+heap.print_heap()
+
+print("Current Queue:")
+heap.print_heap()
+
+served = heap.remove_min()
+print(f"Served patient: {served.name}")  
+
+print("Queue after removal:")
+heap.print_heap()
